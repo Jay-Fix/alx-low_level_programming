c542a50750bcf476d62d88631fc8b1048d78158b
Jay-Fix <abimbolajohn52@gmail.com>
2022-04-07 18:08:11 -0700

    my task 4 commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main is the entry point
+ *stdio is there because of the puts function
+ *Return: 0 after printng the function
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
