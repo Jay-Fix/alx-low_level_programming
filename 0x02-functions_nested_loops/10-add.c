4ee83d5d3529cf9c6a178e47b668dc104ad4f192
Jay-Fix <abimbolajohn52@gmail.com>
2022-04-12 17:52:38 -0700

    my task 10 commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * Add - add two integars and return the sum
+ * @n1: integar 1
+ * @n2: integar 2
+ * Description: give trwo integars and return sum
+ *
+ * Return: sum
+ */
+
+int add(int n1, int n2)
+{
+int sum;
+
+sum = n1 + n2;
+
+return (sum);
+}
