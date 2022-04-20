4d57d8785cd0876683dda21954b6fe97243cfc4c
Jay-Fix <abimbolajohn52@gmail.com>
2022-04-20 12:58:54 -0700

    my task 1 commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integars
+ * @a: input
+ * @b: input
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+int temp;
+
+temp = *a;
+*a = *b;
+*b = temp;
+}
