e0faac14f46c05a9c74030cac04aa30ceb94b36e
John Abimbola <64611054+Jay-Fix@users.noreply.github.com>
2022-05-21 02:36:54 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - a function that checks for lowercase character
- * @C: contains value to be compared
+ * @c: contains value to be compared
  * Return: Always 0
  */
 int _islower(int c)
