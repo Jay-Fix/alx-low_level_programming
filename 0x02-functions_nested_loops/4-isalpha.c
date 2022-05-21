7ed2fb0ec05d291a950ffd0a8ca63652a21668a4
John Abimbola <64611054+Jay-Fix@users.noreply.github.com>
2022-05-21 02:43:07 +0100

    my task 4 commit

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,13 +3,13 @@
 /**
  * _isalpha - Return 1 if c is a letter whether loweercase or uppercase
  * @c: The int to print
- * Return: ALways 0.
+ * Return: Always 0.
  */
 
 int _isalpha(int c)
 
 {
-if ((c > a && c < z) || (c > A && c < Z))
+if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 {
 return (1);
 }
